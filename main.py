0ba4bdd6037cb8d91b8971c51ff32f63c86f19c3
Harsheys21 <jha.harsh21@gmail.com>
2024-05-17 15:45:14 -0700

    push final changes

--- main.py
+++ main.py
@@ -67,28 +67,28 @@ def main():
 
     # ---------------------------------------------------------------------------
 
-    if (test):
-        with open('test.txt', 'r') as f:
-            test_text = [tokenize(line) for line in f.readlines()]
-            indices = feat_extractor.transform_list(test_text)
-            print("train perplexity:", feat_extractor.perplexity(indices))
-            return
+    # if (test):
+    #     with open('test.txt', 'r') as f:
+    #         test_text = [tokenize(line) for line in f.readlines()]
+    #         indices = feat_extractor.transform_list(test_text)
+    #         print("train perplexity:", feat_extractor.perplexity(indices))
+    #         return
     # output accuracy
     print("===== %s Perplexity Scores =====" % args.feature)
-    with open('data/1b_benchmark.train.tokens', 'r') as f:
+    with open('data/1b_benchmark.train.tokens', 'r', encoding='utf-8') as f:
         test_text = [tokenize(line) for line in f.readlines()]
         indices = feat_extractor.transform_list(test_text)
         print("train perplexity:", feat_extractor.perplexity(indices))
 
-    with open('data/1b_benchmark.dev.tokens', 'r') as f:
+    with open('data/1b_benchmark.dev.tokens', 'r', encoding='utf-8') as f:
         test_text = [tokenize(line) for line in f.readlines()]
         indices = feat_extractor.transform_list(test_text)
         print("dev perplexity:", feat_extractor.perplexity(indices))
 
-    with open('data/1b_benchmark.test.tokens', 'r') as f: # ONLY UNCOMMENT ON FINAL RUN
-         test_text = [tokenize(line) for line in f.readlines()]
-         indices = feat_extractor.transform_list(test_text)
-         print("test perplexity:", feat_extractor.perplexity(indices))
+    with open('data/1b_benchmark.test.tokens', 'r', encoding='utf-8') as f: # ONLY UNCOMMENT ON FINAL RUN
+        test_text = [tokenize(line) for line in f.readlines()]
+        indices = feat_extractor.transform_list(test_text)
+        print("test perplexity:", feat_extractor.perplexity(indices))
 
 
 if __name__ == '__main__':
